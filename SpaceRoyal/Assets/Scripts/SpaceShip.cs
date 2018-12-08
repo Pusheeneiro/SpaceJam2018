6272a0a6d96edc2d5fd905bbc123e7b8ae14b927
Michał Węglarz <michal.weglarz@protonmail.com>
2018-12-08 20:59:36 +0100

    Updated spcae ship script

---
+++ SpaceRoyal/Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour {
+
+    public GameObject spaceShip;
+    public GameObject buttonShipUp;
+    public GameObject buttonShipDown;
+    public GameObject buttonCannonLeft;
+    public GameObject buttonCannonRight;
+    public GameObject buttonCannonShot;
+    public GameObject buttonBarrierLeft;
+    public GameObject buttonBarrierRight;
+    public GameObject buttonBarrierCenter;
+    public GameObject buttonAutodestruct;
+    public GameObject cannon;
+    public GameObject leftBarrier;
+    public GameObject rightBarrier;
+    public GameObject centerBarrier;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        
+    }
+}
