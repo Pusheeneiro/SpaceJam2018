41c7026f9591bfc95ea75e3996a8090aec8833cb
Łukasz <majewskiwhs@gmail.com>
2018-12-09 01:11:33 +0100

    PlayerMove+PLayerInteraction

--- SpaceRoyal/Assets/Scripts/Player2.cs
+++ SpaceRoyal/Assets/Scripts/Player2.cs
@@ -2,31 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1 : MonoBehaviour {
-   private  GameObject player;
-    private Vector2 enemyPlayerPosition2D;
+public class Player2 : MonoBehaviour {
+
+    private GameObject player;
+  
     private Vector2 playerPosition2D;
+    private Vector2 enemyPlayerPosition2D;
     private Rigidbody2D playerBody;
     private Vector2 forceW;
     private GameObject enemyPlayer;
     private Rigidbody2D enemyPlayerBody;
 
+    public Vector2 enemyEscapePosition;
     public float speedHorizontal;
     public float speedVertical;
     public float forceVertical;
     public bool spacePhysic;
-    public Vector2 enemyEscapeVector ;
+
 
     // Use this for initialization
     void Start ()
     {
-        player = GameObject.Find("Player1");
-        enemyPlayerPosition2D = GameObject.Find("Player2").transform.position; ;
-        enemyPlayer = GameObject.Find("Player2");
+        player = GameObject.Find("Player2");
+        enemyPlayerPosition2D = GameObject.Find("Player1").transform.position;
+        enemyPlayer = GameObject.Find("Player1");
         enemyPlayerBody = enemyPlayer.GetComponent<Rigidbody2D>();
-
-        playerBody = player.GetComponent<Rigidbody2D>();
         playerPosition2D = new Vector2(0, 0);
+        
+
+
+       
+        playerBody = player.GetComponent<Rigidbody2D>();
         forceW = new Vector2(0, forceVertical);
 
         if (spacePhysic == false)
@@ -38,56 +44,56 @@ public class Player1 : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+
         if (spacePhysic == false)
         {
-             playerPosition2D.x = 0;
-             if (Input.GetKeyDown(KeyCode.W)) 
-              {
-                 playerBody.AddForce(forceW);
-              }
+            playerPosition2D.x = 0;
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                playerBody.AddForce(forceW);
+            }
         }
 
 
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             playerPosition2D.y = speedVertical;
             playerPosition2D.x = 0;
         }
 
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             playerPosition2D.y = 0;
             playerPosition2D.x = -speedHorizontal;
 
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             playerPosition2D.y = -speedVertical;
             playerPosition2D.x = 0;
 
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             playerPosition2D.y = 0;
             playerPosition2D.x = speedHorizontal;
 
         }
-
-       
-        
-
     }
 
     void FixedUpdate()
     {
+        //enemyPlayerPosition2D = enemyEscapePosition;
+        // enemyPlayerPosition2D = new Vector2(100, 100);
+ 
 
         if (spacePhysic == true)
         {
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
             {
                 playerBody.MovePosition(playerBody.position + playerPosition2D * Time.fixedDeltaTime);
             }
@@ -100,15 +106,15 @@ public class Player1 : MonoBehaviour {
                 playerBody.MovePosition(playerBody.position + playerPosition2D * Time.fixedDeltaTime);
             }
         }
-       
+
     }
 
     void OnTriggerEnter2D(Collider2D Other)
     {
-        if (Other.gameObject.name == "Player2")
+        if (Other.gameObject.name == "Player1")
         {
-
-            enemyPlayerBody.transform.position = enemyEscapeVector;
+           
+            enemyPlayerBody.transform.position = enemyEscapePosition;
         }
     }
 }
